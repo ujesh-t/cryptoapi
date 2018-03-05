cd0aa503946bd03a599d15dee814acf14b10281b
Ujesh L T <ut001@nam.ad.pwcinternal.com>
2018-03-05 19:06:13 +0530

    Converted to List

--- arbi.php
+++ arbi.php
@@ -73,9 +73,11 @@ foreach($bbnsJson as $bit) {
                     }
                 }
             }            
+            $coinData = array();
+            $coinData[$coin]['CEX'] = $cexData;
+            $coinData[$coin]['BINANCE'] = $binanceData;
             
-            $output[$coin]['CEX'] = $cexData;
-            $output[$coin]['BINANCE'] = $binanceData;
+            array_push($output, $coinData);
         }
     } 
 }
